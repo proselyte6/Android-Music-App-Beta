0cdd50bb2974e981b62d65bc7e40e74503b2289a
Shafkat Haque <shaque4@binghamton.edu>
2015-07-12 15:48:25 -0400

    Update MainActivity.java

--- app/src/main/java/com/symbol/music_beta/MainActivity.java
+++ app/src/main/java/com/symbol/music_beta/MainActivity.java
@@ -4,10 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+    private RadioGroup options;
+    private Button playlistEdit;
+    private Button saveSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        options = (RadioGroup) findViewById(R.id.options);
+        playlistEdit = (Button) findViewById(R.id.playlistEdit);
+        saveSettings = (Button) findViewByID(R.id.saveSettings);
     }
+    
+    public void editPlaylists(View v) {}
+
+    public void changeSettings(View v) {}
 }
